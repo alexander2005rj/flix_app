26ef86df39b5b16d2da9d514ed887e57f620f92c
Alexander Ribeiro <alexander2005rj@gmail.com>
2025-07-03 21:54:22 -0300

    first commit

---
+++ movies/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from movies.models import Movie
+from movies.serializer import MovieSerializer
+
+class MovieCreateListView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
